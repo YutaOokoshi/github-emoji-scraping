6e0e9cdea39fbd7a98de0d9c0cae6d5ef734615e
cossy <yuuta-ookoshi@coo.ne.jp>
2020-04-29 21:51:31 +0900

    問題を作成

--- github-emoji-scraping/main.py
+++ github-emoji-scraping/main.py
@@ -3,10 +3,28 @@ from config import CONFIG
 
 
 def main():
-    g = Github(CONFIG['GITHUB']['access_token'])
+    g = Github(CONFIG['GITHUB']['user'],CONFIG['GITHUB']['pw'])
 
-    for repo in g.get_user().get_repos():
+    cnt=0
+
+    # 問題１：1000件取得でkリうようにしてください
+
+    # 例１）
+    # starrが1つ以上あるリポジトリ一覧をスター数の降順で取得し1つづつ処理を回す （最大1000件出力する）
+    for repo in g.search_repositories(query="stars:>1", order="desc", sort="stars"):
         print(repo.name)
+
+        # 100件出力したらforを抜ける
+        cnt += 1
+        if cnt >= 100:
+            print(f"{cnt}件出力しました。")
+            break
+
+
+    # 問題２：１つ１つのリポジトリのREADME.mdファイルを取得してください。
+    # for xxxxxx
+
+
 
 if __name__ == '__main__':
     main()
